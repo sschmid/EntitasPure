62a832dfb13e71d5de4bfc662334958da73be537
Simon Schmid <mail@sschmid.com>
2017-04-11 14:13:16 +0200

    Using Entitas 0.41.1

--- EntitasPure/EntitasPure/PositionComponent.cs
+++ EntitasPure/EntitasPure/PositionComponent.cs
@@ -5,4 +5,8 @@ public sealed class PositionComponent : IComponent {
 
     public float x;
     public float y;
+
+    public override string ToString() {
+        return "Position(" + x + ", " + y + ")";
+    }
 }
